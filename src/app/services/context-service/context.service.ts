8aeed467d0ba860ded228b386e05870839816e1e
thorongil05 <officialantonio@outlook.com>
2024-06-23 21:15:08 +0200

    build: update dependencies

--- src/app/services/context-service/context.service.ts
+++ src/app/services/context-service/context.service.ts
@@ -3,38 +3,34 @@ import { FavouritesComponent } from 'src/app/home-view/favourites/favourites.com
 import { Gif } from 'src/app/model/gif';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContextService {
+  private _favourites: Gif[] = [];
 
-  private _favourites : Gif[] = []
+  constructor() {}
 
-  constructor() { }
-
-  
-  public get favourites() : Gif[] {
-    return this._favourites
+  public get favourites(): Gif[] {
+    return this._favourites;
   }
 
-  public set favourites(v : Gif[]) {
+  public set favourites(v: Gif[]) {
     this._favourites = v;
   }
 
-  public addFavourite(gif : Gif) {
-    this._favourites.push(gif)
+  public addFavourite(gif: Gif) {
+    this._favourites.push(gif);
   }
 
   public removeFavourite(gif: Gif) {
-    let index_to_remove = -1
-    this._favourites.forEach(function(element, index) {
+    let index_to_remove = -1;
+    this._favourites.forEach(function (element, index) {
       if (element.name == gif.name) {
-        index_to_remove = index
+        index_to_remove = index;
       }
-    })
+    });
     if (index_to_remove >= 0) {
-      this._favourites.splice(index_to_remove, 1)
+      this._favourites.splice(index_to_remove, 1);
     }
   }
-  
-  
 }
